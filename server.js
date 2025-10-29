63d8a5a906fa25a04476f06a89b990b7f05cb76c
alexandre <alexandre.vogt@orange.fr>
2025-10-29 17:04:41 +0100

    chore: deleted port

--- server.js
+++ server.js
@@ -106,8 +106,3 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ error: 'Not found' }));
     }
 });
-
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
