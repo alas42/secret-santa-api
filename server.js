27394685d1787d28b79a3a15084be829b39c7964
alexandre <alexandre.vogt@orange.fr>
2025-10-29 17:10:09 +0100

    chore: heroku guidelines

--- server.js
+++ server.js
@@ -106,3 +106,8 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ error: 'Not found' }));
     }
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
